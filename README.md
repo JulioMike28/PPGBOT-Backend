e5ea7cae62d6c49fea8302ba432721919a4525c6
JulioMike28 <juliomike17@gmail.com>
2020-08-05 23:29:24 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# PPGBOT
+
+Software contém o objetivo de receber as respostas de um formulário no google para exibir as mesmas em formas de gráficos e elaboração de relatorio.
+
---
+++ backend/server.js
@@ -0,0 +1,73 @@
+import express from 'express';
+const { GoogleSpreadsheet } = require('google-spreadsheet')
+const { promisify } = require('util')
+const creds = require('../credentials.json');
+
+const doc = new GoogleSpreadsheet('1QRHjDwPwRxozDFrYJzqLNxEsBhqsnMeYFSM2_SwRT7Q');
+
+
+const app = express()
+
+async function accessSpreadsheet() {
+    let dados = []
+    await doc.useServiceAccountAuth({
+      client_email: creds.client_email,
+      private_key: creds.private_key,
+    });
+  
+    await doc.loadInfo(); // loads document properties and worksheets
+  
+    const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
+    const rows = await sheet.getRows();
+    const header  = await sheet.headerValues;    
+    
+    let i = 0
+    rows.forEach(element => {
+        
+        element._rawData.forEach(el =>{
+            if(el != ''){
+                dados.push({coluna: header[i], value: el})
+            }else{
+                
+            }
+            i++
+        })
+        i=0
+    })
+
+    tratar(dados)
+}
+  
+let dadosTratados = []
+function tratar(dados) {
+    const estados = dados.reduce((obj, {coluna, value}) => {
+      if (!obj[coluna]) obj[coluna] = [];
+      obj[coluna].push(value);
+      return obj;
+    }, {});
+    
+    dadosTratados = Object.keys(estados).map(coluna => {
+      return {
+        coluna,
+        values: estados[coluna]
+      };
+    });
+}
+
+function Timeout() {
+  setTimeout(()=>{
+    console.log('Timeout ativado 30s')
+    accessSpreadsheet();
+    Timeout()
+  },30000)
+}
+
+Timeout()
+
+app.get("/api/excel", (req,res)=>{
+    res.send(dadosTratados);
+})
+
+app.listen(process.env.PORT || 5000)
+
+
