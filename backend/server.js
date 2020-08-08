c42ff6f45508040f30e0d3bf087c72d8bd3aec14
JulioMike28 <juliomike17@gmail.com>
2020-08-08 01:21:03 -0300

    add perspectiva back

--- backend/server.js
+++ backend/server.js
@@ -38,6 +38,7 @@ async function accessSpreadsheet() {
     })
 
     tratar(dados)
+    Perspectiva(dados)
 }
   
 let dadosTratados = []
@@ -64,10 +65,40 @@ function Timeout() {
   },30000)
 }
 
+let objeto = {}
+let dadosPerspectiva = []
+function Perspectiva(dados) {
+  dados.forEach(el=>{
+    if(el.coluna ==="Ano de início no curso do PPGBot:"){
+      objeto.AnoInicio = el.value
+    }
+    if(el.coluna ==="Ano de Titulação:"){
+      objeto.AnoTitulacao = el.value
+    } 
+    if(el.coluna === "Bolsista:"){
+      objeto.Bolsista = el.value
+    }
+    if(el.coluna === "Agência de Fomento:"){
+      objeto.Agencia = el.value
+    }
+    if(objeto.AnoInicio && objeto.AnoTitulacao && objeto.Bolsista && objeto.Agencia){
+      dadosPerspectiva.push(objeto)
+      objeto = {}
+    }
+    
+  })
+
+  console.log(dadosPerspectiva)
+}
+
+accessSpreadsheet()
 Timeout()
 app.use(cors());
 app.get("/api/excel", (req,res)=>{
     res.send(dadosTratados);
+})
+app.get("/api/perspectiva", (req,res)=>{
+  res.send(dadosPerspectiva);
 })
 app.listen(process.env.PORT || 5000)
 
